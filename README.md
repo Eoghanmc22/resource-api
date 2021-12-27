c17321106b1859e81a8059a6a2ff90ce63f0807f
Eoghanmc22 <eoghanmc@icloud.com>
2021-12-27 11:58:12 -0500

    make minestom load the extension correctly

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Resource API's goal is to make adding assets to the game as easy as it is to do anything else with minestom
 
 ## Todo
-Multi Part Entities \
-Fonts \
+Multi Part Entities (maybe separate) \
+Fonts (centering, custom glyphs, width calculation, vertical height fonts, maybe separate) \
+Armor stand visuals
 Sounds \
 Particles \
 Shaders
--- src/test/java/com/mcecraft/resources/testserver/Start.java
+++ src/test/java/com/mcecraft/resources/testserver/Start.java
@@ -5,6 +5,8 @@ import net.minestom.server.Bootstrap;
 public class Start {
 
     public static void main(String[] args) {
+        System.setProperty("minestom.extension.indevfolder.classes", "build/classes/java/main/");
+        System.setProperty("minestom.extension.indevfolder.resources", "build/resources/main/");
         Bootstrap.bootstrap("com.mcecraft.resources.testserver.Main", args);
     }
 }
