645e46c83c20dcfe751ac73697872703f38c2409
Eoghanmc22 <eoghanmc@icloud.com>
2021-12-27 14:06:35 -0500

    rewrite PackServer and Main

--- src/test/java/com/mcecraft/resources/testserver/Main.java
+++ src/test/java/com/mcecraft/resources/testserver/Main.java
@@ -2,7 +2,6 @@ package com.mcecraft.resources.testserver;
 
 import com.mcecraft.resources.DynamicResourcePack;
 import com.mcecraft.resources.ResourceApi;
-import com.mcecraft.resources.mojang.DefaultResourcePack;
 import com.mcecraft.resources.testserver.blocks.Blocks;
 import com.mcecraft.resources.testserver.items.Items;
 import net.minestom.server.MinecraftServer;
@@ -14,13 +13,31 @@ import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.resourcepack.ResourcePack;
 import net.minestom.server.utils.NamespaceID;
-
-import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         MinecraftServer init = MinecraftServer.init();
 
+        initContent();
+
+        String packHash = generatePack();
+        log.info("Pack hash is {}", packHash);
+
+        initMinestom(packHash);
+
+        init.start("0.0.0.0", 25565);
+        PackServer.start("0.0.0.0", 8081);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(MinecraftServer::stopCleanly));
+    }
+
+    private static void initContent() {
         Items.init();
 
         for (NamespaceID item : Items.REGISTRY.keySet()) {
@@ -32,34 +49,30 @@ public class Main {
         for (NamespaceID block : Blocks.REGISTRY.keySet()) {
             System.out.println("Block " + block + " loaded");
         }
+    }
 
+    private static @NotNull String generatePack() {
         DynamicResourcePack resourcePack = ResourceApi.generateResourcePack("A demo resource pack");
+        PackServer.hostPack(resourcePack);
+        return resourcePack.getHash();
+    }
 
-        try {
-            PackServer.run(resourcePack);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    private static void initMinestom(String packHash) {
+        // setup instance
         Instance instance = MinecraftServer.getInstanceManager().createInstanceContainer();
         instance.setChunkGenerator(new ChunkGen());
 
+        // add necessary events
         MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, (event) -> event.setSpawningInstance(instance));
 
         MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent.class, (event) -> {
             Player pl = event.getPlayer();
 
-            pl.setResourcePack(ResourcePack.forced("http://localhost:8081" + PackServer.getPath(), resourcePack.getHash()));
+            pl.setResourcePack(ResourcePack.forced("http://localhost:8081" + PackServer.getPath(packHash), packHash));
             pl.getInventory().addItemStack(Items.TEST1.create());
             pl.getInventory().addItemStack(Items.TEST2.create());
             pl.teleport(new Pos(0, 45, 0));
             pl.setGameMode(GameMode.CREATIVE);
         });
-
-        init.start("0.0.0.0", 25565);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(MinecraftServer::stopCleanly));
-
-        System.out.println(resourcePack.getHash());
     }
 }
--- src/test/java/com/mcecraft/resources/testserver/PackServer.java
+++ src/test/java/com/mcecraft/resources/testserver/PackServer.java
@@ -2,41 +2,85 @@ package com.mcecraft.resources.testserver;
 
 import com.mcecraft.resources.DynamicResourcePack;
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class PackServer {
 
-    private static String path = null;
+    private static final HttpServer httpServer;
+    private static final HashMap<String, String> hostedPacks = new HashMap<>();
+    private static String pathPrefix = "";
+    private static InetSocketAddress addr = null;
 
-    public static void run(@NotNull DynamicResourcePack rp) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("0.0.0.0", 8081), 0);
+    static {
+        try {
+            httpServer = HttpServer.create();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        byte[] resourcePack = rp.getBytes();
-        path = "/" + rp.getHash() + ".zip";
 
-        httpServer.createContext(path, exchange1 -> {
+    public static void start(@NotNull String address, int port) {
+        if (addr != null) {
+            throw new RuntimeException("The PackServer has already been started");
+        }
+
+        addr = new InetSocketAddress(address, port);
+
+        try {
+            httpServer.bind(addr, 0);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> httpServer.stop(5)));
+            httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void hostPack(@NotNull DynamicResourcePack rp) {
+        String contextPath = getContextPath(rp);
+
+        httpServer.createContext(contextPath, getHandler(rp));
+        hostedPacks.put(rp.getHash(), contextPath);
+    }
+
+    private static @NotNull String getContextPath(@NotNull DynamicResourcePack rp) {
+        return "/" + pathPrefix + rp.getHash() + ".zip";
+    }
+
+    private static @NotNull HttpHandler getHandler(@NotNull DynamicResourcePack rp) {
+        byte[] resourcePackBin = rp.getBytes();
+
+        return exchange1 -> {
             try (HttpExchange exchange = exchange1) {
                 if ("GET".equals(exchange.getRequestMethod())) {
-                    exchange.sendResponseHeaders(200, resourcePack.length);
+                    exchange.sendResponseHeaders(200, resourcePackBin.length);
 
                     OutputStream responseBody = exchange.getResponseBody();
-                    responseBody.write(resourcePack, 0, resourcePack.length);
+                    responseBody.write(resourcePackBin, 0, resourcePackBin.length);
                     responseBody.flush();
                 }
             }
-        });
-
-        httpServer.start();
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> httpServer.stop(5)));
+        };
     }
 
-    public static String getPath() {
-        return path;
+    public static @NotNull String getPathPrefix() {
+        return pathPrefix;
+    }
+
+    public static void setPathPrefix(@NotNull String pathPrefix) {
+        PackServer.pathPrefix = pathPrefix;
+    }
+
+    public static @Nullable String getPath(@NotNull String hash) {
+        return hostedPacks.get(hash);
     }
 }
